2685bfd55af334a1cb75a64bcab4273aaac8c967
docolifrei <oliver.freiberg@docplanner.com>
2026-02-24 11:53:57 +0100

    fixed admin dashboard

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from streamlit_gsheets import GSheetsConnection
 import google.generativeai as genai
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 # --- 1. INITIALIZE SESSION STATE (Must be at the very top) ---
 if 'role' not in st.session_state: st.session_state.role = None
